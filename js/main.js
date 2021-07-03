2192c3c8f33045bfeb02dd8584fbfa11cf7cc421
TOR968 <tor968@yahoo.com>
2021-07-03 16:58:05 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-$(document).ready(function () {
-  //E-mail Ajax Send
-  $('form').submit(function () {
-    //Change
-    var th = $(this);
-    $.ajax({
-      type: 'POST',
-      url: 'mail.php', //Change
-      data: th.serialize(),
-    }).done(function () {
-      alert('Thank you!');
-      setTimeout(function () {
-        // Done Functions
-        th.trigger('reset');
-      }, 1000);
-    });
-    return false;
-  });
-});
-
 $(function () {
   $.exitIntent('enable');
   $(document).bind('exitintent', function () {
@@ -3127,3 +3107,23 @@ $(function () {
       );
     })();
   }.call(this);
+
+$(document).ready(function () {
+  //E-mail Ajax Send
+  $('form').submit(function () {
+    //Change
+    var th = $(this);
+    $.ajax({
+      type: 'POST',
+      url: 'mail.php', //Change
+      data: th.serialize(),
+    }).done(function () {
+      alert('Thank you!');
+      setTimeout(function () {
+        // Done Functions
+        th.trigger('reset');
+      }, 1000);
+    });
+    return false;
+  });
+});
