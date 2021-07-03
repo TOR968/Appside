946c36872127f85f6fe9af2e965dae2cf91ca434
TOR968 <tor968@yahoo.com>
2021-07-03 19:50:47 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3117,13 +3117,14 @@ $(document).ready(function () {
       type: 'POST',
       url: 'mail.php', //Change
       data: th.serialize(),
-    }).done(function () {
-      // alert('Thank you!');
-      setTimeout(function () {
-        // Done Functions
-        th.trigger('reset');
-      }, 1000);
     });
+    //   .done(function () {
+    //   // alert('Thank you!');
+    //   setTimeout(function () {
+    //     // Done Functions
+    //     th.trigger('reset');
+    //   }, 1000);
+    // });
     return false;
   });
 });
