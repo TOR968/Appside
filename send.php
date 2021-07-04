ac0623634275a5391983f0136d481f50ff0331a2
TOR968 <tor968@yahoo.com>
2021-07-04 17:23:20 +0300

    Update send.php

--- send.php
+++ send.php
@@ -31,7 +31,7 @@ try {
     $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
 
     // Настройки вашей почты
-    $mail->Host       = 'smtp.gmail.com’'; // SMTP сервера вашей почты
+    $mail->Host       = 'smtp-relay.gmail.com’'; // SMTP сервера вашей почты
     $mail->Username   = 'spamtest227'; // Логин на почте
     $mail->Password   = 'Q123123Q'; // Пароль на почте
     $mail->SMTPSecure = 'ssl';
