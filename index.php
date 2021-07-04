27e95e52cd0e459ce4e69938031e78ccc73bf64e
TOR968 <tor968@yahoo.com>
2021-07-04 14:49:14 +0300

    jq

--- index.php
+++ index.php
@@ -276,7 +276,9 @@
             });
         }
     </script>
-    <script src="js/jquery.min.js"></script>
+    <!-- <script src="js/jquery.min.js"></script> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
     <script src="js/bootstrap.min.js"></script>
     <script src="js/main.js"></script>
     <script>
